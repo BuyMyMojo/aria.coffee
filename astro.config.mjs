cdf26539933c49dcaff31a2939fe3cdba5bccb18
BuyMyMojo <hello@buymymojo.net>
2024-12-20 20:55:43 +1100

    fix(Docker Image): output SSR site when in docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,22 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  output: import.meta.env.DOCKER ? 'server' : 'static',
+  output: process.env.DOCKER_BUILD ? 'server' : 'static',
   site: "https://aria.coffee",
   integrations: [preact(), tailwind(), icon()],
+
   image: {
     domains: ["buymymojo.net", "aria.coffee", "github.com", "githubusercontent.com", "avatars.githubusercontent.com", "camo.githubusercontent.com", "user-images.githubusercontent.com", "private-user-images.githubusercontent.com"],
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
