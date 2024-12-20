2a4ea33de01866f69097becdbede46d5d421a640
BuyMyMojo <hello@buymymojo.net>
2024-12-20 17:34:57 +1100

    feat: :sparkles: Move from FontAwesome to astro-icons + iconify
    Using: https://www.astroicon.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import preact from "@astrojs/preact";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://aria.coffee",
-  integrations: [preact(), tailwind()],
+  integrations: [preact(), tailwind(), icon()],
   image: {
     domains: ["buymymojo.net", "aria.coffee", "github.com", "githubusercontent.com", "avatars.githubusercontent.com", "camo.githubusercontent.com", "user-images.githubusercontent.com", "private-user-images.githubusercontent.com"],
-  }
+  },
 });
