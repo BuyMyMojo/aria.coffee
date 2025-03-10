4053fbad1ea7087ea6e1ba51a3bdea71386ab1ae
Aria <hello+git@buymymojo.net>
2025-03-10 22:36:21 +1100

    Noi more docker build testing

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -3,7 +3,7 @@ import { execSync } from "node:child_process";
 export function remarkModifiedTime() {
   return (tree, file) => {
     const filepath = file.history[0];
-    const result = typeof import.meta.env.PUBLIC_COMMIT === "undefined" ? "Unavailable" : execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = result.toString();
   };
 }
