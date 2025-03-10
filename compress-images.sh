8439519feb409cc99a9f00ac4493ad3b70907890
Aria <hello+git@buymymojo.net>
2025-03-10 12:22:46 +1100

    Fix compress-images.sh

--- compress-images.sh
+++ compress-images.sh
@@ -2,7 +2,7 @@
 
 if command -v jpegoptim >/dev/null 2>&1 ; then
     echo "jpegoptim found"
-    find ./public \( -iname "*.jpg" -o -iname "*.jpeg" \) | jpegoptim -p -P --files-stdin -w $(nproc --all)
+    find ./dist \( -iname "*.jpg" -o -iname "*.jpeg" \) | jpegoptim -p -P --files-stdin -w $(nproc --all)
 else
     echo "jpegoptim was not found! install either with your package manager or get the binary from the github"
     echo "https://github.com/tjko/jpegoptim/releases/latest"
