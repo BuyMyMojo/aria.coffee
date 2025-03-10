68f698aea868a36fabc72bc22f1f2ca21e987ad2
Aria <hello+git@buymymojo.net>
2025-03-10 22:34:00 +1100

    Fix broken docker build from last modified attempt 3

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -3,7 +3,7 @@ import { execSync } from "node:child_process";
 export function remarkModifiedTime() {
   return (tree, file) => {
     const filepath = file.history[0];
-    const result = import.meta.env.PUBLIC_COMMIT === "" ? "Unavailable" : execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    const result = typeof import.meta.env.PUBLIC_COMMIT === "undefined" ? "Unavailable" : execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = result.toString();
   };
 }
