069577600e51e641aeb764524b5fd9c64389466b
aria <hello@aria.coffee>
2025-06-15 02:27:42 +1000

    chore: bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from "astro/config";
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
 
 import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@tailwindcss/vite";
+// import node from "@astrojs/node";
 
 import icon from "astro-icon";
 
@@ -12,42 +13,47 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    output: process.env.DOCKER_BUILD ? "server" : "static",
-    site: "https://aria.coffee",
-    integrations: [preact(), tailwind(), icon(), mdx()],
+  output: process.env.DOCKER_BUILD ? "server" : "static",
+  site: "https://aria.coffee",
+  integrations: [preact(), tailwind(), icon(), mdx()],
 
-    image: {
-        domains: [
-            "buymymojo.net",
-            "aria.coffee",
-            "github.com",
-            "githubusercontent.com",
-            "avatars.githubusercontent.com",
-            "camo.githubusercontent.com",
-            "user-images.githubusercontent.com",
-            "private-user-images.githubusercontent.com",
-            "alyxia.dev",
-            "nanoshinono.me",
-            "ata.moe",
-            "buh.moe",
-            "hayden.moe",
-            "onz.ee",
-            "notnite.com",
-            "erisdump.neocities.org",
-            "espi.me",
-            "alula.me",
-            "sapphic.moe",
-            "calayucu.com",
-            "meow-d.github.io",
-            "girlthi.ng",
-            "lenooby09.tech",
-            "ackwell.au",
-            "ari.express"
-        ],
-    },
-    markdown: {
-        remarkPlugins: [remarkModifiedTime],
-    },
+  image: {
+      domains: [
+          "buymymojo.net",
+          "aria.coffee",
+          "github.com",
+          "githubusercontent.com",
+          "avatars.githubusercontent.com",
+          "camo.githubusercontent.com",
+          "user-images.githubusercontent.com",
+          "private-user-images.githubusercontent.com",
+          "alyxia.dev",
+          "nanoshinono.me",
+          "ata.moe",
+          "buh.moe",
+          "hayden.moe",
+          "onz.ee",
+          "notnite.com",
+          "erisdump.neocities.org",
+          "espi.me",
+          "alula.me",
+          "sapphic.moe",
+          "calayucu.com",
+          "meow-d.github.io",
+          "girlthi.ng",
+          "lenooby09.tech",
+          "ackwell.au",
+          "ari.express"
+      ],
+  },
+
+  markdown: {
+      remarkPlugins: [remarkModifiedTime],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
 
 // Bellow is for when dedicated server is wanted?
