e5cf30613d89327721f4153ac87b00b980016116
aria <hello@aria.coffee>
2025-07-04 16:15:47 +1000

    feat: add matomo & bump versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import matomo from 'astro-matomo';
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
 
 import preact from "@astrojs/preact";
@@ -15,7 +16,27 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   output: process.env.DOCKER_BUILD ? "server" : "static",
   site: "https://aria.coffee",
-  integrations: [preact(), tailwind(), icon(), mdx()],
+  integrations: [
+    preact(),
+    tailwind(),
+    icon(),
+    mdx(),
+    matomo({
+      enabled: import.meta.env.PROD, // Only load in production
+      host: "https://analytics.aria.coffee/",
+      // setCookieDomain: "*.ara.coffee",
+      // trackerUrl: "js/", // defaults to matomo.php
+      // srcUrl: "js/", // defaults to matomo.js
+      siteId: 1,
+      heartBeatTimer: 5,
+      // disableCookies: true,
+      debug: false,
+      // viewTransition: {
+        // contentElement: "main"
+      // }
+    }),
+
+  ],
 
   image: {
       domains: [
